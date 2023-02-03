0eb6843cfc1c29d7c8bcefc089b664f1c5348665
Junior Paula <luke.junnior@icloud.com>
2023-02-03 19:41:26 -0300

    Configuarte file server to render statics files

--- src/router/routes/routes.go
+++ src/router/routes/routes.go
@@ -20,5 +20,8 @@ func BootsrapRoutes(router *mux.Router) *mux.Router {
 		router.HandleFunc(route.URI, route.Func).Methods(route.Method)
 	}
 
+	fileServer := http.FileServer(http.Dir("./assets/"))
+	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fileServer))
+
 	return router
 }
