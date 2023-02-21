3f737d7d296302627f613ef5543de87e2c06f311
Junior Paula <luke.junnior@icloud.com>
2023-02-21 16:14:59 -0300

    Ensure home page load all posts from user logged

--- src/controllers/pages.go
+++ src/controllers/pages.go
@@ -1,7 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/models"
+	"webapp/src/requests"
+	"webapp/src/responses"
 	"webapp/src/utils"
 )
 
@@ -14,5 +20,24 @@ func SignupPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	utils.Render(w, "home.html", nil)
+	url := fmt.Sprintf("%s/posts", config.APIURL)
+	resp, err := requests.MakeRequestWithAuthentication(r, http.MethodGet, url, nil)
+	if err != nil {
+		responses.ResponseJON(w, http.StatusInternalServerError, responses.ErrorAPI{Error: err.Error()})
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		responses.VerifyStatusCodeErrors(w, resp)
+		return
+	}
+
+	var posts []models.PostDTO
+	if err = json.NewDecoder(resp.Body).Decode(&posts); err != nil {
+		responses.ResponseJON(w, http.StatusUnprocessableEntity, responses.ErrorAPI{Error: err.Error()})
+		return
+	}
+
+	utils.Render(w, "home.html", posts)
 }
