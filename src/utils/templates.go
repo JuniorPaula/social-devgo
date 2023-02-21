6f9fdca8991815d31cb3bdc82eb96023b713ef58
Junior Paula <luke.junnior@icloud.com>
2023-02-21 18:06:25 -0300

    Create templates for views

--- src/utils/templates.go
+++ src/utils/templates.go
@@ -1,14 +1,15 @@
 package utils
 
 import (
+	"html/template"
 	"net/http"
-	"text/template"
 )
 
 var templates *template.Template
 
 func LoadTemplates() {
 	templates = template.Must(template.ParseGlob("views/*.html"))
+	templates = template.Must((templates.ParseGlob("views/templates/*.html")))
 }
 
 func Render(w http.ResponseWriter, template string, data interface{}) {
